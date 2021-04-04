d239f10f016f490419f50752e481417ccd63f81e
Nikolay Tropin <tropinnikolay@gmail.com>
2021-04-04 23:09:17 +0300

    Added some new solutions

--- hw07.hs
+++ hw07.hs
@@ -1,14 +1,19 @@
 -- В Хаскеле есть тип данных 'Map k v', сопостовляющий каждому ключу 
 -- типа @k@ какое-то конкретное значение типа @v@.
-import Data.Map.Strict (Map)
+{-# LANGUAGE RecordWildCards #-}
+
+import Data.Foldable (find, toList)
 import Data.List (foldl')
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import GHC.Float (int2Float)
 
 -- 1. Реализуйте исключительно с помощью свёрток:
 
 -- | 1.1. Функция, суммирующая квадраты элементов списка (0.1 б)
 --
 sumOfSquares :: Num a => [a] -> a
-sumOfSquares = foldr (\thunk elem -> thunk + elem ^ 2) 0
+sumOfSquares = foldl' (\thunk elem -> thunk + elem ^ 2) 0
 
 -- | 1.2. Функция разворачивания списка (0.25 б)
 --
@@ -18,8 +23,8 @@ reverse' = foldl' (\xs x -> x:xs) []
 -- | 1.3. Функция, которая достаёт из списка элемент по индексу (0.25 б)
 --
 
---getByIndex :: [a] -> Int -> a
---getByIndex = foldl' ()
+getByIndex :: [a] -> Int -> Maybe a
+getByIndex list index = snd $ foldl' (\a@(curr, ac) e -> if curr == index then (curr + 1, Just e) else (curr + 1, ac)) (0, Nothing) list
 
 -- | Тип данных "Студент"
 --
@@ -36,40 +41,85 @@ data StudentsLog
       { studentNames :: [String]  -- список имён студентов
       , worstGrade   :: Maybe Int -- наименьшапя оценка по курсу
       , bestGrade    :: Maybe Int -- наибольшая оценка по курсу
-      }
+      } deriving (Show)
 
 -- | 1.4. Функция, которая по списку студентов курса рассчитывает информацию по курсу (0.5 б)
 --
+
 calculateStudentsLog :: [Student] -> StudentsLog
-calculateStudentsLog = undefined
+calculateStudentsLog = foldl' f (StudentsLog [] Nothing Nothing)
+  where
+    f StudentsLog {..} Student {..} =
+      let wg = maybe grade (minimum grade) worstGrade
+          bg = maybe grade (maximum grade) bestGrade
+       in StudentsLog (studentNames ++ [name]) (Just wg) (Just bg)
+    minimum x y = if x < y then x else y
+    maximum x y = if x < y then y else x
 
 -- | 1.5. Функция, которая вставляет в уже отсортированный список элементов
 --        новый элемент на такую позицию, что все элементы левее будут меньше или
 --        равны нового элемента, а все элементы справа будут строго больше (1 б)
 --
+
 insert :: Ord a => [a] -> a -> [a]
-insert = undefined
+insert xs x = foldr aux ini xs False
+  where
+    aux y f done
+      | done || x > y = y : f done
+      | otherwise = x : y : f True
+    ini True = []
+    ini _ = [x]
 
 -- | 1.6. Сортировка вставками. В реализации можно использовать функцию @insert@, 
 --        если вы её реализовали (0.5 б)
 --
+
 insertionSort :: Ord a => [a] -> [a]
-insertionSort = undefined
+insertionSort = foldl' insert []
 
 -- | 1.7. Функция zip. В секции where дана подсказка — вид инициального значения (1 б)
 --
+
 zip' :: [a] -> [b] -> [(a, b)]
-zip' = undefined
+zip' = foldr f ini
   where
     ini :: [b] -> [(a, b)]
     ini _ = []
+    f x xs (y : ys) = (x, y) : xs ys
+    f _ _ [] = []
 
 -- | 2. Сделайте 'StudentsLog' представителем класса типов 'Monoid' и реализуйте
 --      @calculateStudentsLog'@, которая делает то же самое, что и @calculateStudentsLog@.
 --      В реализации нужно использовать то, что 'StudentsLog' — моноид. (0.5 б)
 --
+
+instance Semigroup StudentsLog where
+  s1 <> s2 =
+    StudentsLog
+      (studentNames s1 <> studentNames s2)
+      (worst (worstGrade s1) (worstGrade s2))
+      (best (bestGrade s1) (bestGrade s2))
+
+worst :: Maybe Int -> Maybe Int -> Maybe Int
+worst (Just x) (Just y) = Just $ min x y
+worst Nothing (Just y) = Just y
+worst (Just x) Nothing = Just x
+worst Nothing Nothing = Nothing
+
+best :: Maybe Int -> Maybe Int -> Maybe Int
+best (Just x) (Just y) = Just $ max x y
+best Nothing (Just y) = Just y
+best (Just x) Nothing = Just x
+best Nothing Nothing = Nothing
+
+instance Monoid StudentsLog where
+  mempty = StudentsLog [] Nothing Nothing
+
 calculateStudentsLog' :: [Student] -> StudentsLog
-calculateStudentsLog' = undefined
+calculateStudentsLog' =
+  foldl (\acc s -> acc <> toStudentLog s) mempty
+  where
+    toStudentLog Student {..} = StudentsLog [name] (Just grade) (Just grade)
 
 -- | Хорошо знакомый нам тип данных "Дерево"
 --
@@ -77,6 +127,12 @@ data Tree a = Node a [Tree a] | Leaf
   deriving (Eq, Show)
 
 -- 3. Сделайте 'Tree' представителем класса типов 'Foldable' (1 б)
+
+instance Foldable Tree where
+  foldMap f Leaf = mempty
+  foldMap f n = foldr (\e a -> mappend (f e) a) mempty n
+  foldr f acc Leaf = acc
+  foldr f acc (Node x t) = foldr (flip (foldr f)) (f x acc) t
 
 -- | Тип данных "Яблоко"
 --
@@ -87,31 +143,40 @@ data Apple
       }
   deriving (Eq, Show)
 
+instance Ord Apple where
+  compare app1 app2 = compare (weight app1) (weight app2)
+  app1 <= app2 = (weight app1) <= (weight app2)
+
 -- | 4. С помощью функйций из 'Data.Foldable' реализуйте следующие функции:
 
--- | 4.1. Проверка, что все яблоки в дереве имеют вес, который находится 
+-- | 4.1. Проверка, что все яблоки в дереве имеют вес, который находится
 --        в заданном диапазоне весов (0.1 б)
 --
+
 applesInRange :: Tree Apple -> (Float, Float) -> Bool
-applesInRange = undefined
+applesInRange tree (ls, rs) = all (\Apple {..} -> weight >= ls && weight <= rs) tree
 
 -- | 4.2. Находит яблоко с наибольшим весом (0.1 б)
 --
+
 heaviestApple :: Tree Apple -> Maybe Apple
-heaviestApple = undefined
+heaviestApple Leaf = Nothing
+heaviestApple t = Just . maximum $ t
 
 -- | 4.3 Находит яблоко с цветом из заданного списка цветов и весом,
 --       находящимся в заданном диапазоне весов (0.1 б)
 --
-thisApple :: Tree Apple -> [String] -> (Int, Int) -> Maybe Apple
-thisApple = undefined
 
+thisApple :: Tree Apple -> [String] -> (Int, Int) -> Maybe Apple
+thisApple Leaf _ _ = Nothing
+thisApple app colors (l, r) = find (\Apple {..} -> elem color colors && int2Float l <= weight && weight <= int2Float r) app
 
 -- | 4.4 Считает сумму весов всех яблок в дереве.
 --       В реализации нужно использовать 'Data.Foldable.sum' (0.25 б)
 --
+
 sumOfApples :: Tree Apple -> Float
-sumOfApples = undefined
+sumOfApples t = sum $ weight <$> toList t
 
 -- | Корзинка с яблоками.
 --   Важно, что яблоки в корзинке расфасованы по цветам.
@@ -126,8 +191,13 @@ newtype Basket = Basket { apples :: Map String [Apple] }
 --      инициализировать и модифицировать мапу (0.5 б)
 --
 
---collectBasket :: Apple Tree -> Basket
---collectBasket = undefined
+collectBasket :: Tree Apple -> Basket
+collectBasket =
+  Basket . foldl f mempty
+  where
+    f acc e = case Map.lookup (color e) acc of
+      Just x -> Map.update (Just . flip insert e) (color e) acc
+      Nothing -> Map.insert (color e) [e] acc
 
 -- | Двоичная куча (https://neerc.ifmo.ru/wiki/index.php?title=Двоичная_куча)
 --
